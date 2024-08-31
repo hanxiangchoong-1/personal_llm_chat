412db6dd8b6f74e414e2a29f2f2d96ffc618d134
HanXiangChoong <han@MAINs-MacBook-Pro.local>
2024-08-31 15:27:57 +0800

    Fix sidebar, remove columns

--- main.py
+++ main.py
@@ -10,6 +10,20 @@ import json
 from datetime import datetime
 load_dotenv()
 
+def set_page_container_style():
+    st.markdown(
+        f"""
+        <style>
+            .sidebar .sidebar-content {{
+                position: fixed;
+                overflow-y: auto;
+                height: 100vh;
+            }}
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
 
 # Elasticsearch setup
 es_endpoint = os.environ.get("ELASTIC_ENDPOINT")
@@ -21,42 +35,41 @@ es_client = Elasticsearch(
 LLM = AzureOpenAIClient()
 
 st.set_page_config(layout="wide")
+set_page_container_style()
 
-main_content, padding, right_options = st.columns([3, 0.15, 1])
 
-# LEFT SIDEBAR
-st.sidebar.title("Chat Settings")
-
-model = st.sidebar.radio(
-    "Select LLM",
-    ("gpt-4o", "gpt-4o-mini"),
-    index=0
-)
-
-system_prompt = st.sidebar.radio(
-    "Select System Prompt",
-    (i for i in list(prompts.keys())),
-    index=0  
-)
-
-total_word_count = count_words_in_conversation(st.session_state.get('messages', []), len(st.session_state.get('messages', [])))
-
-conversation_length = st.sidebar.slider("Conversation History Length", min_value=1, max_value=30, value=10, step=1)
-
-word_count = count_words_in_conversation(st.session_state.get('messages', []), conversation_length)
-st.sidebar.markdown(f"""
-    Words in selected history: 
-    <p style='font-size:28px; font-weight:bold; color:#4CAF50; background-color:#E8F5E9; padding:5px 10px; border-radius:2px; display:inline-block;'>{word_count}</p>
-    """, unsafe_allow_html=True)
-
-st.sidebar.markdown(f"""
-    Total words in conversation: 
-    <p style='font-size:28px; font-weight:bold; color:#2196F3; background-color:#E3F2FD; padding:5px 10px; border-radius:2px; display:inline-block;'>{total_word_count}</p>
-    """, unsafe_allow_html=True)
-
-# RIGHT SIDEBAR
 selected_indices=[]
-with right_options:
+# LEFT SIDEBAR
+with st.sidebar:
+    st.title("Chat Settings")
+
+    model = st.radio(
+        "Select LLM",
+        ("gpt-4o", "gpt-4o-mini"),
+        index=0
+    )
+
+    system_prompt = st.radio(
+        "Select System Prompt",
+        (i for i in list(prompts.keys())),
+        index=0  
+    )
+
+    total_word_count = count_words_in_conversation(st.session_state.get('messages', []), len(st.session_state.get('messages', [])))
+
+    conversation_length = st.slider("Conversation History Length", min_value=1, max_value=30, value=10, step=1)
+
+    word_count = count_words_in_conversation(st.session_state.get('messages', []), conversation_length)
+    st.markdown(f"""
+        Words in selected history: 
+        <p style='font-size:28px; font-weight:bold; color:#4CAF50; background-color:#E8F5E9; padding:5px 10px; border-radius:2px; display:inline-block;'>{word_count}</p>
+        """, unsafe_allow_html=True)
+
+    st.markdown(f"""
+        Total words in conversation: 
+        <p style='font-size:28px; font-weight:bold; color:#2196F3; background-color:#E3F2FD; padding:5px 10px; border-radius:2px; display:inline-block;'>{total_word_count}</p>
+        """, unsafe_allow_html=True)
+
     st.title("Elasticsearch")
     if st.button("Clear Chat History"):
         st.session_state.messages = []
@@ -103,33 +116,33 @@ with right_options:
 
 
 # CHAT WINDOW 
-with main_content:
-    if 'messages' not in st.session_state:
-        st.session_state.messages = []
+# with main_content:
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
 
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(f"{message['content']}")
-            st.caption(f"Sent at {message['time']}")
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(f"{message['content']}")
+        st.caption(f"Sent at {message['time']}")
 
-    if prompt := st.chat_input("Start Chatting!"):
-        with st.chat_message("user"):
-            st.markdown(prompt)
-            st.caption(f"Sent at {get_current_time()}")
+if prompt := st.chat_input("Start Chatting!"):
+    with st.chat_message("user"):
+        st.markdown(prompt)
+        st.caption(f"Sent at {get_current_time()}")
 
-        with st.spinner("Generating Response..."):
+    with st.spinner("Generating Response..."):
+        
+        if rag_mode and selected_indices:
+            # RAG mode
+            elasticsearch_results = get_elasticsearch_results(es_client, prompt, selected_indices, es_size)
+            RAG_context = create_RAG_context(elasticsearch_results, prompt)
+            st.session_state.messages.append({"role": "user", "content": prompt, "RAG_context": RAG_context, "time": get_current_time()})
+        else:
+            st.session_state.messages.append({"role": "user", "content": prompt, "RAG_context": "", "time": get_current_time()})
             
-            if rag_mode and selected_indices:
-                # RAG mode
-                elasticsearch_results = get_elasticsearch_results(es_client, prompt, selected_indices, es_size)
-                RAG_context = create_RAG_context(elasticsearch_results, prompt)
-                st.session_state.messages.append({"role": "user", "content": prompt, "RAG_context": RAG_context, "time": get_current_time()})
-            else:
-                st.session_state.messages.append({"role": "user", "content": prompt, "RAG_context": "", "time": get_current_time()})
-                
-            conversation_prompt = create_conversational_prompt(st.session_state.messages, conversation_length=conversation_length)
+        conversation_prompt = create_conversational_prompt(st.session_state.messages, conversation_length=conversation_length)
 
-            assistant_response = LLM.generate_streaming_response(conversation_prompt, model=model, system_prompt=prompts[system_prompt])
+        assistant_response = LLM.generate_streaming_response(conversation_prompt, model=model, system_prompt=prompts[system_prompt])
 
-        st.session_state.messages.append({"role": "assistant", "content": assistant_response, "RAG_context": "", "time": get_current_time()})
-        st.rerun()
+    st.session_state.messages.append({"role": "assistant", "content": assistant_response, "RAG_context": "", "time": get_current_time()})
+    st.rerun()
